5e53a427fa4ca322eaed40ba33112c1528e5653e
Satbir Jhuti <satbirjhuti@gmail.com>
2014-05-10 19:59:08 -0400

    Initial example
    Just put in some example code to get the functions going.

--- uslide.js
+++ uslide.js
@@ -1,3 +1,33 @@
 /* uSlideJS 0.0.1 */
 
+function uSlide() {
+    
+    var params = {};
+    
+    this.init = function(elem, p){
+        params = p;
+        $.ajax({
+            type: p.type,
+            dataType: p.dataType,
+            jsonpCallback: 'parseMedia',
+            url: p.endpoint,
+            data: p.request,
+            cache: true
+        });  
+    }
+    
+    window.parseMedia = function(data) {
+        var mediaObj = params.response.mediaObject.split('.');
+        console.log(mediaObj.length);
+    }
+}
 
+$.fn.extend({
+    'uSlide': function (p) {
+        this.each(function () {
+            var ss = new uSlide(); /* Create an instance of the uSlide class */
+            ss.init(this.id, p); /* Call the init function for each slideshow */
+        });
+        return $(this);
+    }
+});
