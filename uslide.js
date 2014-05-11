aa612dc807fba755657ac834d249bea6084bbb0c
Satbir Jhuti <satbirjhuti@gmail.com>
2014-05-11 14:05:17 -0400

    parseMedia and parseMediaPaths
    - Cleaned out some looping code
    - Added a new function parseMediaPaths to parse individual media
    objects and build an array with paths to individual media objects.

--- uslide.js
+++ uslide.js
@@ -2,7 +2,9 @@
 
 function uSlide() {
     
-    var params = {};
+    var params = {},
+        mediaArray = [],
+        mediaPathsArray = [];
     
     this.init = function(elem, p){
         params = p;
@@ -17,8 +19,27 @@ function uSlide() {
     }
     
     window.parseMedia = function(data) {
-        var mediaObj = params.response.mediaObject.split('.');
-        console.log(mediaObj.length);
+        mediaArray = data;
+        if (params.response.mediaObject.length > 0) {
+            var mediaObjLoc = params.response.mediaObject.split('.');
+            $.each(mediaObjLoc, function( index, value ) {
+                mediaArray = mediaArray[value];
+            });
+        }
+        parseMediaPaths(mediaArray);
+    }
+    
+    var parseMediaPaths = function(media) {
+        mediaPathsArray = media;
+        $.each(media, function( index, value ) {
+            if (params.response.mediaURL.length > 0) {
+                var mediaURLLoc = params.response.mediaURL.split('.');
+                $.each(mediaURLLoc, function( i, val ) {
+                    mediaPathsArray[index] = value[val] + params.response.mediaURLSuffix;
+                });
+            }
+        });
+        console.log(mediaPathsArray);
     }
 }
 
